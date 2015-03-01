8f4d6753afdf5cc04a209556eb8ccb6a17af726a
benmarley <benmarley@Bens-MacBook-Pro-3.local>
2015-03-01 17:22:43 -0500

    added "finished" component: strikethrough, grey text

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
 var $form = $('.form');
 var $item = $('.item');
 var $list = $('.list');
+var $li = $('li')
+
 
 $form.on('submit', function (e) { 
 	e.preventDefault();
-
 
 	var $li = $('<li>').html($item.val());
 
 	$list.append($li);
 
-	
+});
 
+
+$list.on('click', function () {
+	$list.css({
+		'color': 'grey',
+		'text-decoration': 'line-through',
+	});
 });
 
 
