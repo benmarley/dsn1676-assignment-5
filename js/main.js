aea2f6c1b42e517dd88b118d4dc074884566451f
benmarley <benmarley@Bens-MacBook-Pro-3.local>
2015-03-02 15:30:48 -0500

    'X' button now works, strike works

--- js/main.js
+++ js/main.js
@@ -2,19 +2,38 @@ var $form = $('.form');
 var $item = $('.item');
 var $list = $('.list');
 
+
 $form.on('submit', function (e) { 
 	e.preventDefault();
 
 	var $li = $('<li>').html($item.val());
-
+	var $button = $('<button>X</button>');
+	
 	$list.append($li);
-});
+	$li.append($button);
 
-$list.on('click', function () {
-	$list.css({
-		'color': 'grey',
-		'text-decoration': 'line-through',
+	$button.css ({
+		'background-color': '#DDDDDD',
+		'margin-right': '0px',
+		'font-family': "Brush Script MT",
+		'font-size': '1rem',
+		'border': 'solid 1px',
+		'border-color': '#ccc',
+		'position': 'relative',
+		'left': '60%',
+		'border-radius': '5px',
+
 	});
 });
+
+$list.on('click', 'li', function () {
+	$(this).toggleClass('line');
+})
+	
+$list.on('click', 'button', function () {
+	$(this).parent().remove();
+})
+
+
 
 
